441cf5986137d062581a634bd7b7d08cc191854c
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2018-01-25 13:21:51 -0700

    Initial Commit

---
+++ Consle Output/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Consle Output
+//
+//  Created by Berrett, Calvin on 1/25/18.
+//  Copyright © 2018 Berrett, Calvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
